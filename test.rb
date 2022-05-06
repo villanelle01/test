73ba84641111b18e407d6d3ca759ab1f04b9fa13
joker <villaneller@gmail.com>
2022-05-06 21:16:03 +0300

    Add symbols unicodes

--- test.rb
+++ test.rb
@@ -1,8 +1,23 @@
 # encoding: utf-8
+#
+# Программа, которая выбирает произвольную карту игральной колоды, версия 2.
+# Масти — юникод-символы.
+#
+# (с) rubyrush.ru
 
-# Массивы с наборами мастей и достоинств
-values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
-suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'] # Буби, Черви, Крести, Пики
+# Этот код необходим только при использовании unicode-символов на Windows
+if Gem.win_platform?
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
 
-# Выведем произвольную карты, выбрав по одному элементу из массивов
-puts "#{values.sample} of #{suits.sample}"
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
+
+values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
+# Создадим массив с набором мастей, вместо названия масти — иконка-символ
+suits = ['♦', '♠', '♥ ', '♣']
+
+puts "#{values.sample}#{suits.sample}"
